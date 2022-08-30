693528b87f442f6b654f6dca9596e1ea6a900f01
stephen-rebner <stephenrebner@gmail.com>
2022-08-30 18:27:45 +0100

    Create camel-thrase.vb

---
+++ camel-thrase.vb
@@ -0,0 +1,29 @@
+'#Language "WWB-COM"
+
+Sub Main
+	Dim result As String 'a variable to store the final result
+	Dim dictationAsArray() A String array which will hold the dictation as a collection of strings
+
+	'ListVar1 stores what we have just dictated as a string
+	'so use split function to separate all of the individual words into an array
+	dictationAsArray = Split(ListVar1, " ")
+
+	'loop through all of the words. Ensure that the first word is lowercase. The rest of the words should be uppercase
+	For i = 0 To UBound(dictationAsArray)
+		
+		Dim currentWord As String
+
+		currentWord = dictationAsArray(i)
+
+		If i = 0 Then
+			currentWord = LCase(Left(currentWord, 1)) & Mid(currentWord, 2)
+		Else
+			currentWord = UCase(Left(currentWord, 1)) & Mid(currentWord, 2)
+		End If
+
+		result += currentWord
+	Next
+
+	'output the result
+	SendDragonKeys result
+End Sub
